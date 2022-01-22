2d14eb76123cd106072575bc6e06a190f34080b1
belinjo1 <bleerim12@hotmail.com>
2022-01-22 17:08:48 -0500

    Rate-Limit security practice

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const userRouter = require('./Routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,6 +11,14 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+const Ratelimiter = rateLimit({
+    max: 50, //max 100 requests allowed per ip
+    windowMs: 60 * 60 * 1000, // 100 reqs for 1 hr
+    message: 'To many requests from this ip, try again in 1 hr!'
+});
+
+app.use('/api', Ratelimiter);
 
 app.use(express.json());
 
