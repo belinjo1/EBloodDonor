6d4a12f63b13cf712bcb80cc63e9d4f713174727
rilindtasholli <rilindtasholli@gmail.com>
2022-02-09 20:24:45 +0100

    created navbar as a component

--- web-app/src/main.js
+++ web-app/src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faUser, faUsers, faMapMarkerAlt, faHistory, faMedal, faPen } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faHome, faUser, faUsers, faMapMarkerAlt, faHistory, faMedal, faPen)
 
 new Vue({
   router,
